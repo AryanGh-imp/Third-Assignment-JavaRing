91c8819e0b2c01ba4bd9cdc9905d5c80ace111a1
AryanGh-imp <aryan.ghasemi.imp@gmail.com>
2025-03-24 05:28:55 +0330

    Entity.java updated

--- Java-Ring/src/main/java/org/project/entity/Entity.java
+++ Java-Ring/src/main/java/org/project/entity/Entity.java
@@ -15,7 +15,11 @@ public interface Entity {
 
     int getMaxMP();
 
-    /*
-    TODO: ADD OTHER REQUIRED AND BONUS METHODS
-    */
+    String getName();
+
+    boolean isAlive();
+
+    int getCurrentHP();
+
+    int getCurrentMP();
 }
